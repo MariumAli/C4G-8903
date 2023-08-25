37ad782b3f2c0256d42e883cfedd337dfe2fb6a8
Marium Ali <mali328@gatech.edu>
2023-08-26 00:46:08 +0100

    updating google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,8 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "844640573338-sapsnsiuocmal1gll5a4ds4sel6rlv4f.apps.googleusercontent.com",
-            clientSecret: "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",
-            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+            clientId: "537650012750-6sqv9he1jge676e9l3darmcbeb5fndi0.apps.googleusercontent.com",
+            clientSecret: "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",
         })
     ],
     jwt: {
