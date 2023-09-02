1d7de89a7fd744c3e2832a93007364526d9a2395
Marium Ali <mali328@gatech.edu>
2023-09-02 18:41:31 +0100

    DB delete and export functionality added

---
+++ pages/api/removeAllRecords.js
@@ -0,0 +1,25 @@
+import executeQuery from "@/lib/db";
+
+
+export default async function handler(req, res) {
+    try {
+        const db_result = await executeQuery(
+            {
+                query: "TRUNCATE TABLE Application_Applicant_LRO",
+            }
+        );
+
+        if (db_result.hasOwnProperty("affectedRows")) {
+            if (db_result.affectedRows != 0) {
+                res.status(200).json({ result: `Removed all records` })
+            } else {
+                res.status(200).json({ result: `No records found to be deleted.` })
+            }
+        } else {
+            res.status(400).json({ result: "Error found in remote database response, refresh the page and try again." })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ result: error })
+    }
+}
